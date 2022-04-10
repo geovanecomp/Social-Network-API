0117d193b8904415251aa9e7a07bb02ef0ed2f5c
Geovane Pacheco <geovane.pacheco99@gmail.com>
2022-04-09 23:53:21 -0300

    Posterr API app created

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # External Apps
     'rest_framework',
+    'posterr_api.apps.PosterrApiConfig',
 ]
 
 MIDDLEWARE = [
---
+++ posterr_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PosterrApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'posterr_api'
