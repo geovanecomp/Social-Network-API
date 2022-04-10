e4d0e2b9f27cca89f54d4de4460108b993c7162a
Geovane Pacheco <geovane.pacheco99@gmail.com>
2022-04-10 16:54:25 -0300

    Implementing User search for name, email and username

--- posterr_api/views.py
+++ posterr_api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import filters
 from posterr_api import serializers
 from posterr_api import models
 
@@ -7,3 +8,5 @@ class UserViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserSerializer
     queryset = models.User.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email', 'username',)
