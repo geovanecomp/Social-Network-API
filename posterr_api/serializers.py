6583745a5e6b8d8c90d4cad79192e6b52d70caa4
Geovane Pacheco <geovane.pacheco99@gmail.com>
2022-04-10 01:57:57 -0300

    User Serializer

---
+++ posterr_api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from posterr_api import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializes a user object"""
+
+    class Meta:
+        model = models.User
+        fields = ('id', 'name', 'email', 'username', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
