e24b10aa60f6361aa6f4e2e7a647985d6f095c8a
Geovane Pacheco <geovane.pacheco99@gmail.com>
2022-04-10 17:59:12 -0300

    Adding missed permission_classes

--- posterr_api/views.py
+++ posterr_api/views.py
@@ -6,6 +6,7 @@ from rest_framework.settings import api_settings
 
 from posterr_api import serializers
 from posterr_api import models
+from posterr_api import permissions
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -13,6 +14,7 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserSerializer
     queryset = models.User.objects.all()
     authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email', 'username',)
 
@@ -20,3 +22,15 @@ class UserViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+#
+#
+# class UserProfileFeedViewSet(viewsets.ModelViewSet):
+#     """Handles creating, reading and updating profile feed items"""
+#     authentication_classes = (TokenAuthentication,)
+#     serializer_class = serializers.ProfileFeedItemSerializer
+#     queryset = models.ProfileFeedItem.objects.all()
+#     permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+#
+#     def perform_create(self, serializer):
+#         """Sets the user profile to the logged in user"""
+#         serializer.save(user_profile=self.request.user)
