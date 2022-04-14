376be7315b41811cb797840622b0412a323a61b8
Geovane Pacheco <geovane.pacheco99@gmail.com>
2022-04-14 01:42:28 -0300

    Permissions to now allow users to update and delete posts

--- posterr_api/permissions.py
+++ posterr_api/permissions.py
@@ -5,8 +5,19 @@ class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to edit their own profile"""
 
     def has_object_permission(self, request, view, obj):
-        """Check user is trying to edit their own profile"""
+        """Check if user is trying to edit their own profile"""
         if request.method in permissions.SAFE_METHODS:
             return True
 
         return obj.id == request.user.id
+
+
+class DoesNotAllowPostUpdate(permissions.BasePermission):
+    """Does not let users to update any post"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check if user is trying to edit posts"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return False
