aa364aa991db944b91d68b104a2bbc6745b8ff8f
Geovane Pacheco <geovane.pacheco99@gmail.com>
2022-04-14 01:43:10 -0300

    Removing old PostViewSet and updating HomepageViewSet

--- posterr_api/views.py
+++ posterr_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -14,12 +14,12 @@ from posterr_api import permissions
 class UserViewSet(viewsets.ModelViewSet):
     """Handle creating and updating user profiles"""
     serializer_class = serializers.UserSerializer
-    queryset = models.User.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter, filters.OrderingFilter,)
     search_fields = ('name', 'email', 'username',)
     ordering_fields = ['name', 'email']
+    queryset = models.User.objects.all()
 
 
 class UserLoginApiView(ObtainAuthToken):
@@ -27,19 +27,12 @@ class UserLoginApiView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class PostViewSet(viewsets.ModelViewSet):
-    """Handle creating and updating user posts"""
-    serializer_class = serializers.PostSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-    queryset = models.Post.objects.all()
-
-
-class FeedViewSet(viewsets.ReadOnlyModelViewSet):
+class HomepageViewSet(viewsets.ModelViewSet):
     """Show all posts sorted by latest and paginated in the feed as read only"""
     pagination_class = paginations.StandardResultsSetPagination
     serializer_class = serializers.PostSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly, permissions.DoesNotAllowPostUpdate,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('text',)
     queryset = models.Post.objects.order_by('created_at').reverse()
