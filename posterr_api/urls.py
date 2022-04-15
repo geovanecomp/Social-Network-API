cc0cdf26b8095e90d2f16cc74a1289f8403dd103
Geovane Pacheco <geovane.pacheco99@gmail.com>
2022-04-15 19:28:53 -0300

    URL test namespace created

--- posterr_api/urls.py
+++ posterr_api/urls.py
@@ -4,11 +4,15 @@ from rest_framework.routers import DefaultRouter
 
 from posterr_api import views
 
+# This is useful for the url in the automated tests
+app_name = 'api'
 
+# Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register('user', views.UserViewSet, basename='user')
+router.register('userprofile', views.UserViewSet, basename='user')
 router.register('homepage', views.HomepageViewSet, basename='homepage')
 
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
